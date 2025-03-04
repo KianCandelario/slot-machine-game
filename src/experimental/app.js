93d988280a5840090b67a7f31206a150776a97e7
Kian Candelario <kiancandelario1@gmail.com>
2025-03-04 22:16:30 +0800

    CI: Added the background image

--- src/experimental/app.js
+++ src/experimental/app.js
@@ -8,8 +8,15 @@ const devtools_1 = require("@pixi/devtools");
     (0, devtools_1.initDevtools)({ app });
     await app.init({
         resizeTo: window,
-        backgroundColor: 0xffea00
     });
     app.canvas.style.position = "absolute";
+    // Created the texture to make sure that the background is available for use
+    const texture = await pixi_js_1.Assets.load('../assets/background/bg.jpg');
+    const background = new pixi_js_1.Sprite(texture); // Created a sprite to allow PixiJS to display the bg
+    // Set the width and height of the background to the width and height of the window
+    background.height = window.innerHeight;
+    background.width = window.innerWidth;
+    // Add the background to the stage
+    app.stage.addChild(background);
     document.body.appendChild(app.canvas);
 })();
--- src/experimental/app.ts
+++ src/experimental/app.ts
@@ -14,10 +14,20 @@ import { initDevtools } from "@pixi/devtools"
 
     await app.init({
         resizeTo: window,
-        backgroundColor: 0xffea00
     })
 
     app.canvas.style.position = "absolute";
+
+    // Created the texture to make sure that the background is available for use
+    const texture = await Assets.load('../assets/background/bg.jpg')
+    const background = new Sprite(texture)  // Created a sprite to allow PixiJS to display the bg
+
+    // Set the width and height of the background to the width and height of the window
+    background.height = window.innerHeight
+    background.width = window.innerWidth
+
+    // Add the background to the stage
+    app.stage.addChild(background)
 
     document.body.appendChild(app.canvas)
 })()
