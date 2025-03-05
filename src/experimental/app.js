06f007755f533e32a7f8b7ca4bb5a02a6d895708
Kian <kiancandelario1@gmail.com>
2025-03-05 09:00:31 +0800

    feat: Added moving clouds

--- src/experimental/app.js
+++ src/experimental/app.js
@@ -10,13 +10,41 @@ const devtools_1 = require("@pixi/devtools");
         resizeTo: window,
     });
     app.canvas.style.position = "absolute";
-    // Created the texture to make sure that the background is available for use
-    const texture = await pixi_js_1.Assets.load('../assets/background/bg.jpg');
-    const background = new pixi_js_1.Sprite(texture); // Created a sprite to allow PixiJS to display the bg
+    // ---------------
+    // styling functions
+    // ---------------
+    // Adding BG
+    const background = await texture('../assets/background/bg.jpg');
     // Set the width and height of the background to the width and height of the window
     background.height = window.innerHeight;
     background.width = window.innerWidth;
     // Add the background to the stage
     app.stage.addChild(background);
+    // ---------------
+    // Moving Clouds
+    const clouds_texture = await texture('../assets/background/cloud_bg-removebg-preview.png');
+    const moving_clouds = tiling_sprite(clouds_texture.texture);
+    let elapsedTime = 0;
+    const horizontalSpeed = 1; // Horizontal movement speed
+    app.ticker.add((ticker) => {
+        elapsedTime += ticker.deltaTime;
+        // Horizontal movement (parallax scrolling)
+        moving_clouds.tilePosition.x += horizontalSpeed;
+    });
+    // Add the background to the stage
+    app.stage.addChild(moving_clouds);
     document.body.appendChild(app.canvas);
+    // ---------------
+    // Functions
+    // Texture function to make sure that the background is available for use
+    async function texture(asset) {
+        const texture = await pixi_js_1.Assets.load(asset);
+        return new pixi_js_1.Sprite(texture); // Created a sprite to allow PixiJS to display the bg
+    }
+    function tiling_sprite(texture) {
+        return new pixi_js_1.TilingSprite({
+            texture,
+            width: app.screen.width
+        });
+    }
 })();
--- src/experimental/app.ts
+++ src/experimental/app.ts
@@ -2,9 +2,14 @@
 import { 
     Application,
     Assets,
-    Sprite
+    Sprite,
+    Texture,
+    TilingSprite,
+    Filter
  } from "pixi.js"
 import { initDevtools } from "@pixi/devtools"
+import { DropShadowFilter } from '@pixi/filter-drop-shadow'; 
+
 
 
 (async () => {
@@ -18,10 +23,14 @@ import { initDevtools } from "@pixi/devtools"
 
     app.canvas.style.position = "absolute";
 
-    // Created the texture to make sure that the background is available for use
-    const texture = await Assets.load('../assets/background/bg.jpg')
-    const background = new Sprite(texture)  // Created a sprite to allow PixiJS to display the bg
+    // ---------------
+    // styling functions
+    
+        
 
+    // ---------------
+    // Adding BG
+    const background = await texture('../assets/background/bg.jpg')
     // Set the width and height of the background to the width and height of the window
     background.height = window.innerHeight
     background.width = window.innerWidth
@@ -29,5 +38,42 @@ import { initDevtools } from "@pixi/devtools"
     // Add the background to the stage
     app.stage.addChild(background)
 
+
+    // ---------------
+    // Moving Clouds
+    const clouds_texture = await texture('../assets/background/cloud_bg-removebg-preview.png')
+    const moving_clouds = tiling_sprite(clouds_texture.texture)
+
+    let elapsedTime = 0
+    const horizontalSpeed = 1   // Horizontal movement speed
+
+    app.ticker.add((ticker) => {
+
+        elapsedTime += ticker.deltaTime
+        // Horizontal movement (parallax scrolling)
+        moving_clouds.tilePosition.x += horizontalSpeed
+    })
+    // Add the background to the stage
+    app.stage.addChild(moving_clouds)
+
+
+
     document.body.appendChild(app.canvas)
+
+
+
+    // ---------------
+    // Functions
+    // Texture function to make sure that the background is available for use
+    async function texture(asset: string) {
+        const texture = await Assets.load(asset)
+        return new Sprite(texture)  // Created a sprite to allow PixiJS to display the bg
+    }
+
+    function tiling_sprite(texture: Texture) {
+        return new TilingSprite({ 
+            texture,
+            width: app.screen.width
+         })
+    }
 })()
