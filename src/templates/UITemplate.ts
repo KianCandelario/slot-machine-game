4e9f32f5b95e80b84eed80aeb9444b1955e150d6
Kian <kiancandelario1@gmail.com>
2025-03-13 09:48:07 +0800

    wip: Started the template for buttons

---
+++ src/templates/UITemplate.ts
@@ -0,0 +1,13 @@
+import { Component } from "../core/Component";
+
+export class DecreaseButton extends Component   {
+    constructor() {
+        super()
+    }
+
+    public async init(): Promise<void> {
+
+    }
+
+    public update(): void {}
+}
